c06cb3702b5d220ed8772e67f83ce5742a653a6a
Amey Parulekar <amey@wirywolf.com>
2012-04-22 00:56:48 +0530

    Cleaned up globals and math constants.

--- src/player.js
+++ src/player.js
@@ -1,8 +1,13 @@
-var Keycodes = {
-    UP: 38,
-    DOWN: 40,
-    RIGHT: 39,
-    LEFT: 37
+var PlayerGlobals = {
+    Keycodes: {
+	UP: 38,
+	DOWN: 40,
+	RIGHT: 39,
+	LEFT: 37
+    },
+    Constants: {
+	sqrt2: 1.414
+    }
 };
 
 function Player(gs) {
@@ -22,97 +27,105 @@ function Player(gs) {
 
     var r = gs.width * 0.01;
 
-    return {
-	update: function() {
-	    if (vy === 0) {
-		pos.x += vx;
-	    }
-	    else if (vx === 0) {
-		pos.y -= vy;
+    var update = function() {
+	if (vy === 0) {
+	    pos.x += vx;
+	}
+	else if (vx === 0) {
+	    pos.y -= vy;
+	}
+	else {
+	    pos.x += vx/PlayerGlobals.Constants.sqrt2;
+	    pos.y -= vy/PlayerGlobals.Constants.sqrt2;
+	}
+    };
+
+    var draw = function(c) {
+	c.fillRect(pos.x - r/2, pos.y - r/2, r, r);
+    };
+
+    var handleKeyDown = function(code) {
+	if (code === PlayerGlobals.Keycodes.UP ){
+	    if (moveDown === false) {
+		vy = WALK_VY;
 	    }
 	    else {
-		pos.x += vx/1.414;
-		pos.y -= vy/1.414;
+		vy = 0;
 	    }
-	},
-	draw: function(c) {
-	    c.fillRect(pos.x - r/2, pos.y - r/2, r, r);
-	},
-	keyDown: function(code) {
-	    if (code === Keycodes.UP ){
-		if (moveDown === false) {
-		    vy = WALK_VY;
-		}
-		else {
-		    vy = 0;
-		}
-		moveUp = true;
-	    }
-	    else if (code === Keycodes.DOWN) {
-		if (moveUp === false) {
-		    vy = -WALK_VY;
-		}
-		else {
-		    vy = 0;
-		}
-		moveDown = true;
-	    }
-	    else if (code === Keycodes.LEFT) {
-		if (moveRight === false) {
-		    vx = -WALK_VX;
-		}
-		else {
-		    vx = 0;
-		}
-		moveLeft = true;
-	    }
-	    else if (code === Keycodes.RIGHT) {
-		if (moveLeft === false) {
-		    vx = WALK_VX;
-		}
-		else {
-		    vx = 0;
-		}
-		moveRight = true;
-	    }
-	},
-	keyUp: function(code) {
-	    if (code === Keycodes.UP ){
-		if (moveDown === true) {
-		    vy = -WALK_VY;
-		}
-		else {
-		    vy = 0;
-		}
-		moveUp = false;
-	    }
-	    else if (code === Keycodes.DOWN) {
-		if (moveUp === true) {
-		    vy = WALK_VY;
-		}
-		else {
-		    vy = 0;
-		}
-		moveDown = false;
-	    }
-	    else if (code === Keycodes.LEFT) {
-		if (moveRight === true) {
-		    vx = WALK_VX;
-		}
-		else {
-		    vx = 0;
-		}
-		moveLeft = false;
-	    }
-	    else if (code === Keycodes.RIGHT) {
-		if (moveLeft === true) {
-		    vx = -WALK_VX;
-		}
-		else {
-		    vx = 0;
-		}
-		moveRight = false;
-	    }
+	    moveUp = true;
 	}
+	else if (code === PlayerGlobals.Keycodes.DOWN) {
+	    if (moveUp === false) {
+		vy = -WALK_VY;
+	    }
+	    else {
+		vy = 0;
+	    }
+	    moveDown = true;
+	}
+	else if (code === PlayerGlobals.Keycodes.LEFT) {
+	    if (moveRight === false) {
+		vx = -WALK_VX;
+	    }
+	    else {
+		vx = 0;
+	    }
+	    moveLeft = true;
+	}
+	else if (code === PlayerGlobals.Keycodes.RIGHT) {
+	    if (moveLeft === false) {
+		vx = WALK_VX;
+	    }
+	    else {
+		vx = 0;
+	    }
+	    moveRight = true;
+	}
+    };
+
+    var handleKeyUp = function(code) {
+	if (code === PlayerGlobals.Keycodes.UP ){
+	    if (moveDown === true) {
+		vy = -WALK_VY;
+	    }
+	    else {
+		vy = 0;
+	    }
+	    moveUp = false;
+	}
+	else if (code === PlayerGlobals.Keycodes.DOWN) {
+	    if (moveUp === true) {
+		vy = WALK_VY;
+	    }
+	    else {
+		vy = 0;
+	    }
+	    moveDown = false;
+	}
+	else if (code === PlayerGlobals.Keycodes.LEFT) {
+	    if (moveRight === true) {
+		vx = WALK_VX;
+	    }
+	    else {
+		vx = 0;
+	    }
+	    moveLeft = false;
+	}
+	else if (code === PlayerGlobals.Keycodes.RIGHT) {
+	    if (moveLeft === true) {
+		vx = -WALK_VX;
+	    }
+	    else {
+		vx = 0;
+	    }
+	    moveRight = false;
+	}
+    };
+
+    return {
+	update: update,
+	draw: draw,
+	keyDown: handleKeyDown,
+	keyUp: handleKeyUp
     };
 }
