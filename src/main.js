831611437552b727998a349a41c6d81184858062
Amey Parulekar <amey@wirywolf.com>
2012-04-23 03:13:45 +0530

    Corrected widths and heights of blocks in order to not draw outside the canvas.

--- src/main.js
+++ src/main.js
@@ -20,14 +20,14 @@ function startGame() {
 			width: 40
 		    }
 		],
-		levelStart: true,
+		levelStart: false,
 		tiles: [
 		    [0, 0, gs.width, 20],
 		    [0, 0, 20, gs.height/1.5-20],
 		    [0, gs.height/1.5+20, 20, gs.height/3],
-		    [0, gs.height-20, gs.width, gs.height],
-		    [gs.width-20, 0, gs.width, gs.height/3-20],
-		    [gs.width-20, gs.height/3+20, gs.width, gs.height]
+		    [0, gs.height-20, gs.width, 20],
+		    [gs.width-20, 0, 20, gs.height/3-20],
+		    [gs.width-20, gs.height/3+20, 20, gs.height/1.5-20]
 		]
 	    },
 	    {
@@ -44,9 +44,9 @@ function startGame() {
 		tiles: [
 		    [0, 0, gs.width, 20],
 		    [0, 0, 20, gs.height/1.5-20],
-		    [0, gs.height/1.5+20, 20, gs.height],
-		    [0, gs.height-20, gs.width, gs.height],
-		    [gs.width-20, 0, gs.width, gs.height]
+		    [0, gs.height/1.5+20, 20, gs.height/3-20],
+		    [0, gs.height-20, gs.width, 20],
+		    [gs.width-20, 0, 20, gs.height]
 		]
 	    },
 	    {
@@ -59,13 +59,13 @@ function startGame() {
 			width: 40
 		    }
 		],
-		levelStart: false,
+		levelStart: true,
 		tiles: [
 		    [0, 0, gs.width, 20],
 		    [0, 0, 20, gs.height],
-		    [0, gs.height-20, gs.width, gs.height],
-		    [gs.width-20, 0, gs.width, gs.height/2-20],
-		    [gs.width-20, gs.height/2+20, gs.width, gs.height]
+		    [0, gs.height-20, gs.width, 20],
+		    [gs.width-20, 0, 20, gs.height/2-20],
+		    [gs.width-20, gs.height/2+20, 20, gs.height/2-20]
 		]
 	    }
 	]
