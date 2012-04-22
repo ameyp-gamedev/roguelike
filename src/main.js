02651ab6351f0b81e4e79037f467962a77d1f0be
Amey Parulekar <amey@wirywolf.com>
2012-04-23 02:46:47 +0530

    Moved portal to the left wall to verify that it works too.

--- src/main.js
+++ src/main.js
@@ -13,17 +13,17 @@ function startGame() {
 		    },
 		    {
 			to: "level1:room3",
-			at: [gs.width, gs.height/1.5-20, gs.width, gs.height/1.5+20]
+			at: [0, gs.height/1.5-20, 0, gs.height/1.5+20]
 		    }
 		],
 		levelStart: true,
 		tiles: [
 		    [0, 0, gs.width, 20],
-		    [0, 0, 20, gs.height],
+		    [0, 0, 20, gs.height/1.5-20],
+		    [0, gs.height/1.5+20, 20, gs.height/3],
 		    [0, gs.height-20, gs.width, gs.height],
 		    [gs.width-20, 0, gs.width, gs.height/3-20],
-		    [gs.width-20, gs.height/3+20, gs.width, gs.height/3-40],
-		    [gs.width-20, gs.height/1.5+20, gs.width, gs.height]
+		    [gs.width-20, gs.height/3+20, gs.width, gs.height]
 		]
 	    },
 	    {
@@ -48,16 +48,16 @@ function startGame() {
 		portals: [
 		    {
 			to: "level1:room1",
-			at: [0, gs.height/2-20, 0, gs.height/2+20]
+			at: [gs.width, gs.height/2-20, gs.width, gs.height/2+20]
 		    }
 		],
 		levelStart: false,
 		tiles: [
 		    [0, 0, gs.width, 20],
-		    [0, 0, 20, gs.height/2-20],
-		    [0, gs.height/2+20, 20, gs.height],
+		    [0, 0, 20, gs.height],
 		    [0, gs.height-20, gs.width, gs.height],
-		    [gs.width-20, 0, gs.width, gs.height]
+		    [gs.width-20, 0, gs.width, gs.height/2-20],
+		    [gs.width-20, gs.height/2+20, gs.width, gs.height]
 		]
 	    }
 	]
